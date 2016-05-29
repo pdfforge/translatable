3fe03475adc071d3c8e0d3e8a7eed5c7d56efd09
philip.chinery <philip.chinery@pdfforge.org>
2016-05-29 16:56:36 +0200

    Refactor TranslationFactory to do reflection stuff instead of TranslationSource

--- Translation/TranslationFactory.cs
+++ Translation/TranslationFactory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
+using System.Reflection;
 
 namespace Translation
 {
@@ -23,9 +25,73 @@ namespace Translation
         {
             var instance = Activator.CreateInstance<T>();
 
-            _translationSource.Translate(instance);
+            Translate(instance, _translationSource);
 
             return instance;
+        }
+
+        private void Translate(ITranslatable o, TranslationSource translationSource)
+        {
+            var type = o.GetType();
+            var translationSection = type.FullName;
+
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            IPluralBuilder pluralBuilder = new DefaultPluralBuilder();
+
+            foreach (var property in properties)
+            {
+                if (!property.CanRead || !property.CanWrite)
+                    continue;
+
+                if (property.PropertyType == typeof(string))
+                {
+                    SetStringProperty(o, property, translationSection, translationSource);
+                    continue;
+                }
+
+                if (property.PropertyType == typeof(string[]))
+                {
+                    SetStringArrayProperty(o, property, translationSection, pluralBuilder, translationSource);
+                    continue;
+                }
+
+                if (property.PropertyType == typeof(IPluralBuilder))
+                {
+                    SetPluralbuilderProperty(o, property, pluralBuilder);
+                    continue;
+                }
+
+                throw new InvalidOperationException($"The type {property.PropertyType} is not supported in ITranslatables.");
+            }
+        }
+
+        private void SetPluralbuilderProperty(ITranslatable translatable, PropertyInfo property, IPluralBuilder pluralBuilder)
+        {
+            property.SetValue(translatable, pluralBuilder);
+        }
+
+        private void SetStringProperty(ITranslatable o, PropertyInfo property, string translationSection, TranslationSource translationSource)
+        {
+            var value = (string)property.GetValue(o);
+
+            var translated = translationSource.GetTranslation(translationSection, value);
+
+            if (!string.IsNullOrEmpty(translated))
+                property.SetValue(o, translated);
+        }
+
+        private void SetStringArrayProperty(ITranslatable o, PropertyInfo property, string translationSection, IPluralBuilder pluralBuilder, TranslationSource translationSource)
+        {
+            var value = (string[])property.GetValue(o);
+
+            if (value.Length != 2)
+                throw new InvalidDataException($"The plural string for section {translationSection} and key {property.Name} must contain two strings: a singular and a plural form. It contained {value.Length} strings.");
+
+            var translations = translationSource.GetAllTranslations(translationSection, value[0], pluralBuilder);
+
+            if (translations.Length == pluralBuilder.NumberOfPlurals)
+                property.SetValue(o, translations);
         }
     }
 }
--- Translation/TranslationSource.cs
+++ Translation/TranslationSource.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Reflection;
 using NGettext;
 using NGettext.Loaders;
 
@@ -55,70 +53,6 @@ namespace Translation
                 return new string[] {};
 
             return translations;
-        }
-
-        public void Translate(ITranslatable o)
-        {
-            var type = o.GetType();
-            var translationSection = type.FullName;
-
-            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-            IPluralBuilder pluralBuilder = new DefaultPluralBuilder();
-
-            foreach (var property in properties)
-            {
-                if (!property.CanRead || !property.CanWrite)
-                    continue;
-
-                if (property.PropertyType == typeof(string))
-                {
-                    SetStringProperty(o, property, translationSection);
-                    continue;
-                }
-
-                if (property.PropertyType == typeof(string[]))
-                {
-                    SetStringArrayProperty(o, property, translationSection, pluralBuilder);
-                    continue;
-                }
-
-                if (property.PropertyType == typeof(IPluralBuilder))
-                {
-                    SetPluralbuilderProperty(o, property, pluralBuilder);
-                    continue;
-                }
-
-                throw new InvalidOperationException($"The type {property.PropertyType} is not supported in ITranslatables.");
-            }
-        }
-
-        private void SetPluralbuilderProperty(ITranslatable translatable, PropertyInfo property, IPluralBuilder pluralBuilder)
-        {
-            property.SetValue(translatable, pluralBuilder);
-        }
-
-        private void SetStringProperty(ITranslatable o, PropertyInfo property, string translationSection)
-        {
-            var value = (string) property.GetValue(o);
-
-            var translated = GetTranslation(translationSection, value);
-
-            if (!string.IsNullOrEmpty(translated))
-                property.SetValue(o, translated);
-        }
-
-        private void SetStringArrayProperty(ITranslatable o, PropertyInfo property, string translationSection, IPluralBuilder pluralBuilder)
-        {
-            var value = (string[])property.GetValue(o);
-
-            if (value.Length != 2)
-                throw new InvalidDataException($"The plural string for section {translationSection} and key {property.Name} must contain two strings: a singular and a plural form. It contained {value.Length} strings.");
-
-            var translations = GetAllTranslations(translationSection, value[0], pluralBuilder);
-
-            if (translations.Length == pluralBuilder.NumberOfPlurals)
-                property.SetValue(o, translations);
         }
     }
 }
